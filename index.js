87f9d55cdece194f0588e8116e9dfc78f25ebc65
UberBooster <info@uberbooster.com>
2016-07-01 19:15:18 -0400

    set up the routes in the server

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ var server = express();
 
 var port = process.env.PORT || 8080;
 
+server.get('/todos', function(request, response){
+  response.send('GET todos');
+});
+
+server.get('/todos/:id', function(request, response){
+  response.send('GET todos :id');
+});
+
+server.post('/todos', function(request, response){
+  response.send('POST todos');
+});
+
+server.put('/todos/:id', function(request, response){
+  response.send('PUT todos :id');
+});
+
+server.delete('/todos/:id', function(request, response){
+  response.send('DELETE todos :id');
+});
+
 server.listen(port, function(){
   console.log('Now listening on port...' + port);
 });
