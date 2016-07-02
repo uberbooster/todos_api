bb4c782f884312407f1f5277722b31da8adf9b18
UberBooster <info@uberbooster.com>
2016-07-01 20:37:34 -0400

    set up POST route and installed uuid

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var lowdb = require('lowdb');
+var uuid = require('uuid');
 var server = express();
 
 var port = process.env.PORT || 8080;
+var db = lowdb('db.json');
+
+//Database Initialization
+db.defaults({todos: []})
+  .value(); //The .value() will run the above commands
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/todos', function(request, response){
   response.send('GET todos');
@@ -12,7 +22,17 @@ server.get('/todos/:id', function(request, response){
 });
 
 server.post('/todos', function(request, response){
-  response.send('POST todos');
+  var todo = {
+    id: uuid.v4(),
+    description: request.body.description,
+    isComplete: false
+  };
+
+  var result = db.get('todos')
+                 .push(todo)
+                 .last()
+                 .value();
+  response.send(result);
 });
 
 server.put('/todos/:id', function(request, response){
